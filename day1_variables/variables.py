ff2c95cacec1dc10f04421db2eb12376175fa666
sujalbaral40 <sujalbaral40@gmail.com>
2025-11-25 18:48:38 +1100

    Create variables.py

---
+++ day1_variables/variables.py
@@ -0,0 +1,15 @@
+print("user profile")
+
+full_name = input("what is your full name: ")
+print(full_name)
+
+age = (int(input("what is your age:" )))
+print(age)
+
+favourite_hobby = input("what is your favourite hobby: ")
+print(favourite_hobby)
+
+dream_job = ("what is your dream job:")
+print(dream_job)
+
+print("thank you")
